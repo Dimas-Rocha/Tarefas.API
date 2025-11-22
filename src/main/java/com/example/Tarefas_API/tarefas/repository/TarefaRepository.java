8f79a6516e00ada3225ccf521177d13167d37038
Dimas Rocha <dimas.dasp@gmail.com>
2025-11-22 18:41:13 -0300

    Repository

---
+++ src/main/java/com/example/Tarefas_API/tarefas/repository/TarefaRepository.java
@@ -0,0 +1,8 @@
+package com.example.Tarefas_API.tarefas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+
+}
